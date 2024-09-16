33a206a188a882bdc63f439ea07396974aa3d90f
thspower <hspowersport@gmail.com>
2024-09-16 12:22:22 -0500

    Update pwaicons.sh

--- pwaicons.sh
+++ pwaicons.sh
@@ -2,9 +2,11 @@
 # There are a set of images which are required for PWAs and PWA to App Store apps
 # This script will, given image (transparency respected) and a color
 # make 192x192px, 512x512px, 512x512 with background color
+#
+# Usage: pwaicons.sh image.png "#123456"
 
 image=$1
-color=${2:-'#ffffff'}
+color=${2:-'#000000'}
 
 magick $image -resize 192x192 "192-${image}"
 magick $image -resize 512x512 "512-${image}"
